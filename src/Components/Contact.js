652645daf39cd0e5cc95bce34bbf2ff1aeb9135c
haizhou85 <haizhou.guo@gmail.com>
2020-03-19 12:53:31 -0700

    remove address

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -80,13 +80,7 @@ class Contact extends Component {
             <aside className="four columns footer-widgets">
                <div className="widget widget_contact">
 
-					   <h4>Address and Phone</h4>
-					   <p className="address">
-						   {name}<br />
-						   {street} <br />
-						   {city}, {state} {zip}<br />
-						   <span>{phone}</span>
-					   </p>
+					   
 				   </div>
 
             </aside>
